0f4b2a69188e38c7f380accb1b0ef6c4a9280b0c
Stan Bienaives <stan.bienaives@gmail.com>
2015-03-21 10:24:33 -0300

    [chile] init front-end

---
+++ examples/read_video.js
@@ -0,0 +1,76 @@
+
+//function clickButton() {
+    //map.featureLayer.eachLayer(function(marker) {
+        //// You can replace this test for anything else, to choose the right
+        //// marker on which to open a popup. by default, popups are exclusive
+        //// so opening a new one will close all of the others.
+        //if (marker.feature.properties.title === 'Capital Pride Parade') {
+            //marker.openPopup();
+        //}
+    //});
+//}
+//var layer = L.mapbox.featureLayer(mapName);
+
+//layer.on('click', function ( e ){
+   //console.log('clicked');
+   //console.log( e.layer.feature );
+//});
+
+//var markers = map.markerLayer.getLayers().map(function(marker) { return marker; })
+
+
+//console.log( markers );
+
+
+//markers.forEach( function ( m ){
+   //m.on('click', function ( ){
+      //console.log('clicked');
+      //console.log( this.feature );
+   //});
+//});
+
+
+
+//.setView([40.82,-73.9],11);
+
+
+   //$scope.$watch('photos', function ( photos, old ){
+
+      //if ( $scope.layer ) {
+         //$scope.layer.clearLayers();
+      //}
+      //if( photos ) {
+         //var geoJson = photos.map(function ( photo ){
+            //return {
+               //type: 'Feature',
+               //geometry: {
+                  //type: 'Point',
+                  //coordinates: [photo.location.lon,photo.location.lat]
+               //},
+               //properties: {
+                  ////title: 'Peregrine Espresso',
+                  //description: "<img height='270px' src='"+ photo.url +"'>",
+                  //// one can customize markers by adding simplestyle properties
+                  //// https://www.mapbox.com/foundations/an-open-platform/#simplestyle
+                  ////'marker-size': 'large',
+
+                  //title: photo.user.full_name,
+                  //image: photo.url,
+                  //url: photo.url,
+                  //'marker-color': '#BE9A6B',
+                  //'marker-symbol': 'camera'
+               //}
+            //}
+         //});
+
+         //$scope.layer =  L.mapbox.markerLayer(geoJson).addTo($scope.map);
+         //$scope.layer.on('click',function ( e ){
+            //console.log(e.layer.feature.properties.url);
+            //console.log( $scope );
+            //$scope.shownImageUrl = e.layer.feature.properties.url;
+            //$scope.$apply();
+         //});
+      //}
+   //});
+//}]);
+
--- front/scripts/map.js
+++ front/scripts/map.js
@@ -1,5 +1,8 @@
 
 
+// TODO REactify!!
+
+
 //var directory = "/Users/sebastienvian/Desktop/photos-iphone/"
 //var directory = "https://s3-eu-west-1.amazonaws.com/la-ballade/videos/"
 
@@ -9,93 +12,88 @@ var mapName = 'stanbienaives.l752j3lk'
 var map = L.mapbox.map('map', mapName )
 
 
+var data;
+
+
+var video = document.getElementsByTagName('video')[0];
+
+video.onended = function (e){
+   playNext();
+};
+
+
+var current_video = 1;
 
 map.featureLayer.on('ready', function(e) {
     //document.getElementById('open-popup').onclick = clickButton;
+    // local store datas
+    data = _.map( map.featureLayer.toGeoJSON().features , function ( f ){ return f.properties;} );
+
+
+    playVideoFromIndex(getCurrentVideo());
+
 
     map.featureLayer.eachLayer(function(marker) {
       marker.on('click', function ( e ){
          console.log('clicked');
          console.log( marker.feature.properties.name );
-         // change video source:
-         //document.querySelector("#video").src = directory +  marker.feature.properties.name;
-         document.querySelector("#video").src = marker.feature.properties.path;
+         playVideo( marker.feature.properties );
       });
     });
 });
 
-//function clickButton() {
-    //map.featureLayer.eachLayer(function(marker) {
-        //// You can replace this test for anything else, to choose the right
-        //// marker on which to open a popup. by default, popups are exclusive
-        //// so opening a new one will close all of the others.
-        //if (marker.feature.properties.title === 'Capital Pride Parade') {
-            //marker.openPopup();
-        //}
-    //});
-//}
-//var layer = L.mapbox.featureLayer(mapName);
-
-//layer.on('click', function ( e ){
-   //console.log('clicked');
-   //console.log( e.layer.feature );
-//});
-
-//var markers = map.markerLayer.getLayers().map(function(marker) { return marker; })
 
 
-//console.log( markers );
+// play video
+var playVideo  = function ( properties ){
+  centerMap( properties.position ); 
+   //startVideo( directory +  properties.name );
+  startVideo( properties.path );
+  // store current_video in cookie
+  setCurrentVideo(properties.index);
+};
+
+// read specific video
+var playVideoFromIndex = function ( index ){
+  props  = _.find( data, function ( properties ){ return properties.index == index } );
+  playVideo( props );
+};
 
 
-//markers.forEach( function ( m ){
-   //m.on('click', function ( ){
-      //console.log('clicked');
-      //console.log( this.feature );
-   //});
-//});
+// read next
+var playNext = function (){
+   current_video++;
+   playVideoFromIndex( current_video );
+};
 
 
+// center map 
+var centerMap = function ( position ){
+   ( position.latitude )? position.lat = position.latitude : null;
+   ( position.longitude )? position.lon = position.longitude : null;
+   // position format: { lat: 'xxx' , lon: 'xxx' }
+   map.panTo( position );
+};
 
-//.setView([40.82,-73.9],11);
 
+// play video
+var startVideo = function ( src ){
+   document.querySelector("#video").src = src;
+};
 
-   //$scope.$watch('photos', function ( photos, old ){
+var setCurrentVideo = function ( index ){
+  current_video = index;
+  document.cookie = 'last_read_video='+ index;
+  console.log( document.cookie );
+};
 
-      //if ( $scope.layer ) {
-         //$scope.layer.clearLayers();
-      //}
-      //if( photos ) {
-         //var geoJson = photos.map(function ( photo ){
-            //return {
-               //type: 'Feature',
-               //geometry: {
-                  //type: 'Point',
-                  //coordinates: [photo.location.lon,photo.location.lat]
-               //},
-               //properties: {
-                  ////title: 'Peregrine Espresso',
-                  //description: "<img height='270px' src='"+ photo.url +"'>",
-                  //// one can customize markers by adding simplestyle properties
-                  //// https://www.mapbox.com/foundations/an-open-platform/#simplestyle
-                  ////'marker-size': 'large',
-
-                  //title: photo.user.full_name,
-                  //image: photo.url,
-                  //url: photo.url,
-                  //'marker-color': '#BE9A6B',
-                  //'marker-symbol': 'camera'
-               //}
-            //}
-         //});
-
-         //$scope.layer =  L.mapbox.markerLayer(geoJson).addTo($scope.map);
-         //$scope.layer.on('click',function ( e ){
-            //console.log(e.layer.feature.properties.url);
-            //console.log( $scope );
-            //$scope.shownImageUrl = e.layer.feature.properties.url;
-            //$scope.$apply();
-         //});
-      //}
-   //});
-//}]);
+var getCurrentVideo = function ( index ){
+   var cookie = document.cookie
+   console.log( cookie );
+   if ( cookie !== '' ) {
+      return cookie.split('=')[1];
+   } else {
+      return current_video;
+   }
+};
 
